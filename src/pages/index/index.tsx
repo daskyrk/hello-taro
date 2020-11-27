09cdeac7a6cac5f1f7aea1f2f7089520bc036b3e
LiJun <lij@terminus.io>
2020-11-27 20:54:04 +0800

    feat: update index logic

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,31 +6,45 @@ import Profile from '../profile/profile';
 import './index.scss'
 
 export default () => {
-  const [activeBar, setActiveBar] = React.useState(0);
+  const [tabs, setTabs] = React.useState([
+    {
+      key: 'recommend',
+      icon: 'tuijian',
+      title: '推荐',
+      content: Gallery,
+    },
+    {
+      key: 'category',
+      icon: 'toushi',
+      title: '分类',
+      content: Category,
+    },
+    {
+      key: 'profile',
+      icon: 'smile',
+      title: '我的',
+      content: Profile,
+    },
+  ])
+  const [activeTab, setActiveTab] = React.useState(tabs[0]);
 
-  const handleSwitchTab = (index) => {
-    setActiveBar(index)
-  }
+  const Content = activeTab.content;
   return (
     <View className='container'>
       <View className="main">
-        {activeBar === 0 && <Gallery />}
-        {activeBar === 1 && <Category />}
-        {activeBar === 2 && <Profile />}
+        {<Content />}
       </View>
       <View className="tab-bar">
-        <View onClick={() => handleSwitchTab(0)} className={`${activeBar === 0 ? 'active' : ''} tab-bar-item`}>
-          <View className='iconfont icon-tuijian'></View>
-          <View>推荐</View>
-        </View>
-        <View onClick={() => handleSwitchTab(1)} className={`${activeBar === 1 ? 'active' : ''} tab-bar-item`}>
-          <Text className='iconfont icon-toushi'></Text>
-          <Text>分类</Text>
-        </View>
-        <View onClick={() => handleSwitchTab(2)} className={`${activeBar === 2 ? 'active' : ''} tab-bar-item`}>
-          <Text className='iconfont icon-smile'></Text>
-          <Text>我的</Text>
-        </View>
+        {
+          tabs.map((tab) => {
+            return (
+              <View key={tab.key} onClick={() => setActiveTab(tab)} className={`${activeTab === tab ? 'active' : ''} tab-bar-item`}>
+                <View className={`iconfont icon-${tab.icon}`}></View>
+                <View>{tab.title}</View>
+              </View>
+            )
+          })
+        }
       </View>
     </View>
   )
