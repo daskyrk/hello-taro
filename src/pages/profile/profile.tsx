f303616290e591732c28d379ff2395d3e7644264
LiJun <lij@terminus.io>
2020-11-29 09:54:25 +0800

    feat: add welcome for special persons

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,7 +1,29 @@
 import { Image, View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import React from "react";
+import config from "../../_config";
 import './profile.scss';
+
+const getWelcomeForSpecial = (userInfo: Taro.UserInfo) => {
+  let welcome = '欢迎';
+  if (!config.specialPersons.includes(userInfo.nickName)) {
+    return welcome
+  }
+
+  const hour = new Date().getHours();
+  const timeRange = [
+    [0, 8, '早睡'],
+    [8, 12, '早安'],
+    [12, 18, '午安'],
+    [18, 24, '晚安'],
+  ]
+  timeRange.forEach(([start, end, str]) => {
+    if (hour >= start && hour < end) {
+      welcome = str as string;
+    }
+  })
+  return welcome;
+}
 
 export default () => {
   const [userInfo, setUserInfo] = React.useState({} as any);
